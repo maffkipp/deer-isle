f839a1fd876f8bc7dce69c68b1d3d48b94768111
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2018-06-29 15:13:25 -0500

    added navbar and navbutton components

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import NavButton from './NavButton';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div className='navbar'>
+            </div>
+        );
+    }
+}
+
+export default NavBar;
---
+++ src/components/NavButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NavButton extends Component {
+    render() {
+        return (
+            <div className='navbutton'>
+            </div>
+        );
+    }
+}
+
+export default NavButton;
