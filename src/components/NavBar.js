3ca058690f5be45d5062aff844ef621b5acd9691
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2018-07-10 23:42:21 -0500

    added navbar styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -32,7 +32,10 @@ export default NavBar;
 //Styled Components
 const Nav = styled.div`
     position: fixed;
+    display: flex;
+    justify-content: end;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
     width: 100%;
     height: 60px;
-    background-color: grey;
+    background-color: #616161;
 `
--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -13,11 +13,17 @@ class NavLink extends Component {
 
 export default NavLink;
 
+// Styled components
 const Anchor = styled.a`
+    display: inline-block;
     text-decoration: none;
+    font-family: 'Lato', sans-serif;
+    font-size: 16px;
     color: white;
     padding: 20px;
+    transition: background-color 200ms;
     &:hover {
-        background-color: green;
+        background-color: #9E9E9E;
+        transition: background-color 200ms;
     }
 `
